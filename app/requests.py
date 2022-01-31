8b97fbff2863407a89200310e94b1550a3e9431a
CosBett <cosmasbett9@gmail.com>
2022-01-31 10:31:19 +0300

    create variables for API and urls and set it to none

---
+++ app/requests.py
@@ -0,0 +1,14 @@
+import urllib.request, json
+from .models import Articles, Source
+
+# Obtaining API key
+api_key = None
+
+# Obtaining base url
+base_url = None
+
+# Obtaining headlines url
+headlines_url = None
+
+# Obtaining everything url
+everything_url = None
