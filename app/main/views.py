c628683c11491f20eef2cd21e57e624d11141ddc
CosBett <cosmasbett9@gmail.com>
2022-01-31 11:34:23 +0300

    add method to display news sources

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,7 @@
-from flask import render_template
-from app import app
+from flask import render_template,request,redirect, url_for
+import app
+from . import main
+from ..requests import get_sources
 
 
 @app.route('/')
@@ -8,6 +10,9 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
+    #  Obtaining News sources
+    news_sources = get_sources()
+    print(news_sources)
 
     title = 'Home - Welcome to Worldwide News Website'
-    return render_template('index.html', title = title)
+    return render_template('index.html', title = title, sources = news_sources)
