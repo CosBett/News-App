d738909526c278185a70b9580a01e47c6b40441a
CosBett <cosmasbett9@gmail.com>
2022-01-31 10:42:24 +0300

    create configure function for requests

--- app/requests.py
+++ app/requests.py
@@ -12,3 +12,10 @@ headlines_url = None
 
 # Obtaining everything url
 everything_url = None
+
+def configure_request(app):
+  global api_key, base_url, headlines_url, everything_url
+  api_key = app. config['NEWS_HIGHLIGHT_API_KEY']
+  base_url = app.config['NEWS_HIGHLIGHT_API_BASE_URL']
+  headlines_url = app.config['NEWS_HEADLINES_URL']
+  everything_url = app.config['NEWS_EVERYTHING_URL']
--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class config:
   General configuration parent class
   '''
   
-  NEWS_TOPHEADLINES_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apikey={}' 
+  NEWS_HEADLINES_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apikey={}' 
   NEWS_HIGHLIGHT_API_BASE_URL = 'https://newsapi.org/v2/sources?apiKey={}'
   NEWS_EVERYTHING_URL = 'https://newsapi.org/v2/everything?q=trending&language=en&apiKey={}'
   NEWS_HIGHLIGHT_API_KEY = os.environ.get('NEWS_HIGHLIGHT_API_KEY')
