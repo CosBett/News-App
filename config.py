48c5ccdb4c8b9a1115deb14ce3500b76f3412611
CosBett <cosmasbett9@gmail.com>
2022-01-31 08:57:53 +0300

    Store News Base url

--- config.py
+++ config.py
@@ -3,7 +3,12 @@ class config:
   '''
   General configuration parent class
   '''
-  pass
+  
+  NEWS_TOPHEADLINES_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apikey={}' 
+  NEWS_HIGHLIGHT_API_BASE_URL = 'https://newsapi.org/v2/sources?apiKey={}'
+  NEWS_EVERYTHING_URL = 'https://newsapi.org/v2/everything?q=trending&language=en&apiKey={}'
+  NEWS_HIGHLIGHT_API_KEY = os.environ.get('NEWS_HIGHLIGHT_API_KEY')
+
 
 class ProdConfig(config):
   '''
