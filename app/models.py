d54b18c2f97a9f53371cfea9b34c7de177298399
CosBett <cosmasbett9@gmail.com>
2022-01-31 10:20:34 +0300

    create and source model class and define its objects

--- app/models.py
+++ app/models.py
@@ -11,3 +11,17 @@ class Articles:
     self.publishedAt = publishedAt
     self.content = content
 
+class Source:
+  '''
+  Source class to define source objects
+  '''
+  def __init__(self, id, name, description, url, category, language, country):
+    self.id = id
+    self.name = name
+    self.description = description
+    self.url = url
+    self.category = category
+    self.language = language
+    self.country = country
+    
+    
