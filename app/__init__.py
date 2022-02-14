7b866ce20318272da241a514a64d50fa7944b5e5
CosBett <cosmasbett9@gmail.com>
2022-02-14 04:42:20 +0300

    add google font CDN link

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,16 @@
 from flask import Flask
 from config import config_options
-from flask_bootstrap import Bootstrap
-from newsapi import NewsApiClient
+
+# from newsapi import NewsApiClient
 
 def create_app(config_name):
   app = Flask(__name__,instance_relative_config=True)
-  newsapi = NewsApiClient
+  # newsapi = NewsApiClient
 # Creating the app configurations
   app.config.from_object(config_options[config_name])
   app.config.from_pyfile('config.py') 
 #Initializing flask extensions
-  Bootstrap(app)
+ 
 
 #registering the blueprint
   from .main import main as main_blueprint
